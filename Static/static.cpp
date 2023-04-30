1d844ab0724a6423b5377f987344ee19653ffdc0
AswinSuresh666 <117917680+ASWINSURESH666@users.noreply.github.com>
2023-04-30 17:17:36 +0530

    Update static.cpp

--- Static/static.cpp
+++ Static/static.cpp
@@ -1,28 +1,62 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 
-class MyClass{
-    private:
-        static int count;
-    public:
-        MyClass(){
-            count++;
-        }
-        static void showcount(){
-            cout<<"Number of objects created: "<<count<<endl;
-        }
+class objects
+{
+private:
+    static int count;
+
+public:
+    void new_object()
+    {
+        count++;
+    }
+    void del_object()
+    {
+        count--;
+    }
+    static void show_count()
+    {
+        cout << "Number of objects created= " << count << endl;
+    }
 };
 
-int MyClass::count = 0;
+int objects::count = 0;
 
-int main(){
-    MyClass obj1, obj2, obj3;
-
-    MyClass::showcount();
-
-    MyClass obj4, obj5;
-
-    obj5.showcount();
-
+int main()
+{
+    int choice;
+    string option;
+    objects object;
+    do
+    {
+        cout << "Enter your choice\n";
+        cout << "1 to add an object\n";
+        cout << "2 to remove an object\n";
+        cout << "3 to display number of objects\n";
+        cout << "4 to quit\n";
+        cin >> choice;
+        switch (choice)
+        {
+        case 1:
+            object.new_object();
+            cout << "\nAn object created\n";
+            break;
+        case 2:
+            object.del_object();
+            cout << "\nAn object removed\n";
+            break;
+        case 3:
+            objects::show_count();
+            break;
+        case 4:
+            return 0;
+        default:
+            cout << "Invalid entry\n";
+        }
+        cout << "Do you want to continue (enter y/n): ";
+        cin >> option;
+    } while (option == "y");
     return 0;
 }
